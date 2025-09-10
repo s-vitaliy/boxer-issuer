0eb32dc9b2f982b84a09d06112b2ee3f69f68f31
Vitalii Savitskii <visa@ecco.com>
2025-09-10 15:28:55 +0200

    Improve error handling in token issuance process (#168)
    * Improve error handling in token issuance process
    
    * Bump boxer-core version

--- src/http/controllers/v1/token.rs
+++ src/http/controllers/v1/token.rs
@@ -1,11 +1,9 @@
-use crate::http::errors::*;
 use crate::models::api::external::identity_provider::ExternalIdentityProvider;
 use crate::models::api::external::token::ExternalToken;
 use crate::services::token_service::{TokenProvider, TokenService};
 use actix_web::web::{Data, Path};
 use actix_web::{get, HttpRequest};
-use boxer_core::services::audit::events::token_validation_event::{TokenValidationEvent, TokenValidationResult};
-use boxer_core::services::audit::AuditService;
+use log::error;
 use std::sync::Arc;
 
 #[utoipa::path(
@@ -16,38 +14,24 @@ use std::sync::Arc;
 )]
 #[get("/token/{identity_provider}")]
 pub async fn token(
-    audit_service: Data<Arc<dyn AuditService>>,
     data: Data<Arc<TokenService>>,
     identity_provider: Path<String>,
     req: HttpRequest,
-) -> Result<String> {
+) -> actix_web::Result<String> {
     let ip = ExternalIdentityProvider::from(identity_provider.to_string());
-    let maybe_token = req.headers().get("Authorization");
-    let maybe_header = maybe_token.clone().map(|header| async move {
-        let token = ExternalToken::try_from(header)?;
-        data.issue_token(ip, token).await
-    });
 
-    let result = match maybe_header {
-        Some(fut) => {
-            let token = fut.await?;
-            Ok(token)
-        }
-        None => Err(Error::new("Unauthorized")),
-    };
-
-    let audit_result = match &result {
-        Ok(_) => TokenValidationResult::Success,
-        Err(ref err) => TokenValidationResult::Failure(err.to_string()),
-    };
-
-    let token_hash = maybe_token
-        .map(|header| format!("{:x}", md5::compute(header)))
-        .unwrap_or("empty".to_string());
-    let event = TokenValidationEvent::external(token_hash, audit_result);
-    audit_service.record_token_validation(event).map_err(|err| {
-        log::error!("Failed to record audit event: {}", err);
-        Error::new("Unauthorized")
+    let header = req.headers().get("Authorization").ok_or_else(|| {
+        error!("Authorization header missing");
+        actix_web::error::ErrorUnauthorized("Unauthorized")
     })?;
-    result
+
+    let token = ExternalToken::try_from(header).map_err(|err| {
+        error!("Invalid token format: {}", err);
+        actix_web::error::ErrorUnauthorized("Unauthorized")
+    })?;
+    let internal_token = data.issue_token(ip, token).await.map_err(|err| {
+        error!("Token issuance failed: {}", err);
+        actix_web::error::ErrorUnauthorized("Unauthorized")
+    })?;
+    Ok(internal_token)
 }
